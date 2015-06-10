5fd6ac008304e1ad0c0398143432d4f972df1faa
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-10 18:17:09 +0100

    Added test to check for capacity method which returns DEFAULT_CAPACITY; created capacity method using attr_reader and added it to the initialize method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,12 @@ require_relative 'bike'
 class DockingStation
   DEFAULT_CAPACITY = 20
 
+  attr_reader :capacity
+
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
-
 
 
   def release_bike
@@ -22,7 +24,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= @capacity
   end
 
   def empty?
