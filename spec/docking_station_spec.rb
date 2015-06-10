f4d17ff97fcb4a3e20ea31a37d00f6c8f33fa489
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-10 17:18:04 +0100

    Added test for error when docking but dock is full; amended dock method to include full error

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 
 describe DockingStation  do
+
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
@@ -14,6 +15,13 @@ describe DockingStation  do
   describe 'release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe 'dock' do
+    it 'raises an error when full' do
+      subject.dock Bike.new
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
