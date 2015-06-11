01238f6650c5b83f737eaf96dc6037271c1a25cc
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-11 17:00:01 +0100

    Added :bike as a pre double replacement in testing to reduce dependency on variables other than what we really want to test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -33,8 +33,8 @@ describe DockingStation  do
 
   describe 'dock' do
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock :bike }
+      expect { subject.dock :bike }.to raise_error 'Docking station full'
     end
   end
 
